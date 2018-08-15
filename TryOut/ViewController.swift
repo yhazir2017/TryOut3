6ed7d223056de31681c37feccf153c049e4c25d0
yhazir2017 <yildiray.hazir@gmail.com>
2018-08-15 13:28:49 -0700

    at time 13:28 updated

--- TryOut/ViewController.swift
+++ TryOut/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("TryOut Tests")
         print("yildiray Hazir")
+        print("githup test at 13:28")
     }
 
 }
