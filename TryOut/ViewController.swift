02a0958519b2e8d83860158f0c3c23b0dcf96556
yhazir2017 <yildiray.hazir@gmail.com>
2018-08-15 13:13:14 -0700

    Name added

--- TryOut/ViewController.swift
+++ TryOut/ViewController.swift
@@ -14,13 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("TryOut Tests")
+        print("yildiray Hazir")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
